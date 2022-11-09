9c8f81ff4ead4cbd15a7dde2a85b5c4cb098ae80
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2022-11-09 00:38:03 -0300

    añadí eventos, creé función constante general

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
+const $ = (selector) => document.querySelector (selector);
+
 //-------variables-------:
+let body = document.body;
+let btn = $("#btn");
+let textoSuperior = $("#texto-superior");
+/*let pTextoSuperior = document.getElementsByClassName("p-texto-superior"); no se usa por ahora*/
+let textoCambiar = $("#texto-cambiar");
+let TextoInferior = $("#texto-inferior");
+let textoInferiorCambiar = $("#texto-inferior-cambiar");
+let btnImagen = $("#btn-imagen");
+let urlInput = $("#url-img");
+let imagenCambiar = $("#img-cambiar");
+let btnTexto = $("#btn-texto");
+let asideImagen  = $(".aside-imagen");
+let contImagen = $("#descargar-img");
+//let imagenPrincipal = $("#imagen-principal");
+let inputColorImagen = $("#input-color-imagen");
+let inputBrillo = $("#brillo");
+let inputOpacidad = $("#opacidad");
+let btnDescargar = $("#btn-descargar");
+let asideTexto = $(".aside-texto");
+let btnRestablecer = $("#btn-restablecer");
+
+/* /-------variables-------:
 let body = document.body;
 let btn = document.getElementById("btn");
 let textoSuperior = document.getElementById("texto-superior");
-/*let pTextoSuperior = document.getElementsByClassName("p-texto-superior"); no se usa por ahora*/
+/*let pTextoSuperior = document.getElementsByClassName("p-texto-superior"); no se usa por ahora
 let textoCambiar = document.getElementById("texto-cambiar");
 let TextoInferior = document.getElementById("texto-inferior");
 let textoInferiorCambiar = document.getElementById("texto-inferior-cambiar");
 let btnImagen = document.getElementById("btn-imagen");
+let urlInput = document.getElementById("url-img");
+let imagenCambiar = document.getElementById("img-cambiar");
 let btnTexto = document.getElementById("btn-texto");
 let asideImagen  = document.querySelector(".aside-imagen");
-let asideTexto = document.querySelector(".aside-texto");
+let asideTexto = document.querySelector(".aside-texto");*/
+
 
 //-------funciones-------:
 
@@ -46,4 +73,35 @@ btnTexto.addEventListener("click", () =>{
 }
 )
 
+//vincular url
+urlInput.addEventListener("input", (event) => {
+  imagenCambiar.style.backgroundImage = `url("${event.target.value}")`;
+}
+)
+//descargar imagen
+const descargarMeme = () => {
+domtoimage.toBlob(contImagen).then(function (blob) {
+        saveAs(blob, "mi-meme.png");
+    });
+  }
 
+  btnDescargar.addEventListener("click", descargarMeme)
+
+  //cambio de color
+
+  inputColorImagen.addEventListener("blur", (event) => {
+imagenCambiar.style.backgroundColor= event.target.value;
+  }
+   );
+   
+   //boton restablecer EVENTO
+
+
+
+   inputBrillo.addEventListener("change", (event) => {
+imagenCambiar.style.filter = `brightness(${event.target.value})`;
+   }
+   )
+   inputOpacidad.addEventListener("change", (event) => {
+    imagenCambiar.style.opacity = `${event.target.value}%`;
+   })
